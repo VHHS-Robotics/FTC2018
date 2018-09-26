5bd36e2b6734575113b31e4660cf8fab96f4dbf2
DESKTOP-9A5FCPM\ivane <ivanegas@transitsystems.biz>
2018-09-25 17:12:15 -0700

    test on laptop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -53,6 +53,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+
 @Autonomous(name="AutoControl", group="Iterative Opmode")
 public class Auto extends OpMode
 {
