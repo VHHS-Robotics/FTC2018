8348f4f30ed57de5855bf3783f35bd76b884863c
VHHS-Robotics <vhhsrobotics@gmail.com>
2018-10-27 08:12:41 -0700

    Add liftmotor control Deleted example code and comments for them

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -38,20 +38,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.*;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 // Roman is my extraterrestrial cat -Luis
 
 @Autonomous(name="AutoControl", group="Iterative Opmode")
@@ -153,9 +139,10 @@ public class Auto extends LinearOpMode
                         break;
             }
 
-
             //move into crater
 
+
+            //Make sure this code does not repeat
             runOnce = false;
         }
     }
